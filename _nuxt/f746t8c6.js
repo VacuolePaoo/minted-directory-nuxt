8743a8e525cfce735859fcb0c4b41e803afa5321
VacuolePaoo <161350352+VacuolePaoo@users.noreply.github.com>
2025-03-16 13:50:54 +0000

    Deploying to gh-pages from @ VacuolePaoo/minted-directory-nuxt@2f25761f306256e3f1347fa6e8b051bdc492b4ca 🚀

---
+++ _nuxt/f746t8c6.js
@@ -0,0 +1,1 @@
+import{j as r,c as t,i as o,S as s}from"./S49a1Kj9.js";const a={};function c(e,n){return o(),t("table",null,[s(e.$slots,"default")])}const f=r(a,[["render",c]]);export{f as default};
